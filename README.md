de7d127662a20f558e2b7c6f1e1680d9fe5e504f
Milad_Salimi <milad.najafgholi.salimi@gmail.com>
2025-11-26 22:22:48 +0330

    Improve Project Code
    I used Tkinter to my project become more user friendly and user works with GUI.
    I also fixed static paths and tried to make them dynamic with Tkinter package.
    Also used Docstrings to be more readable and more easy to understand.
    And at the end I fixed some bugs and make code more smooth.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # To_Do_List
-My first to-do list project through self-learning. You can add your tasks with a priority, and it will automatically sort them accordingly. You can also remove tasks, view them, save your progress, and quit whenever you’d like. I hope you enjoy it — from a junior data scientist.
+My first To-Do List project through self-learning. You can add your tasks with a priority, and it will automatically sort them accordingly. You can also remove tasks, view them, save your progress, and quit whenever you’d like. I hope you enjoy it — from a junior data scientist.
+
+P.S: I just followed steps to solve the problem at the beginning. It was my python project to solve. Not my fun thing to do for myself. But now, because I passed my task and solve the problem as well, I started to modify and change the program to be better.
--- ToDoList_Project.py
+++ ToDoList_Project.py
@@ -1,42 +1,83 @@
-# My simple To Do List project
+# ToDo : Fix creating file problem. Take help from Screenshots.
 
 import csv
+from tkinter import Tk, filedialog
 
+"""
+Import csv to work with csv files. We need to read and save changes of our ToDo List on a csv file.
+CSV stands for Comma-Separated Value.
+
+Import Tk and filedialog from tkinter package to take path directory and select file in a simple GUI.
+GUI stands for Graphical User Interface
+"""
 
 task_list = []
-high_task_list = []
-middle_task_list = []
-low_task_list = []
+high_priority_task_list = []
+middle_priority_task_list = []
+low_priority_task_list = []
 
+"""
+Define "task_list", "high_task_list", "middle_task_list" and "low_task_list" to keep values in continue.
+The goal is to seperate tasks with different priority to sort our To Do list in high to low priority and know which task is 
+more important to do first and which is not.
+
+"high_priority_task_list" is a list of tasks with high priority.
+"middle_priority_task_list" is a list of tasks with middle priority.
+"low_priority_task_list" is a list of tasks with middle priority.
+"""
 
 class Task:
-    def __init__(self, task, description, priority, dict_name):
+    def __init__(self, dict_name, task, description, priority):
+        self.dict_name = dict_name
         self.task = task
         self.description = description
         self.priority = priority.capitalize()
-        self.dict_name = dict_name
+        
 
+"""
+Define a class named "Task" with initial values: dict_name, task, description, priority. 
+
+"dict_name" -- choose a name for the dictionary.
+"task" -- what is your task
+"description" -- Describe what is your task about and what you like to add as a note to remember.
+"priority" -- Your task has a high, middle or low priority.
+"""
 
 class ToDoList(Task):
     def __init__(self, task, description, priority, dict_name):
-        super().__init__(task, description, priority, dict_name)
-
-    def adding_task(self):
+        super().__init__(dict_name, task, description, priority)
+        
+        """
+        a child class named "ToDoList" that inherited from its parent class named "Task".
+        """
+    def add_task(self):
             self.dict_name = {
                 "task": self.task,
                 "description": self.description,
                 "priority": self.priority
                 }
-            if self.dict_name["priority"] in ["High", "Middle", "Low"]:
+            """ 
+            We define a dictionary with following keys:
+                task - task's name
+                description - describe what it is
+                priority - what is the priority of this task? High - Middle or Low
+            """
+
+            if self.dict_name["priority"] in ("High", "Middle", "Low"):
                 task_list.append(self.dict_name)
                 return ("Your task added successfully. \u2705 \n")
             else:
                 return ("Priority is not correct. \u274c")
+
+            """
+            If priority added correctly, task will append to the list.
+            But the priority didn't recognize, will raise a message. 
+            """
     
-    @staticmethod
+    @staticmethod           # used decorator here because don't need class's properties.
     def removing_task():
         remove_task = input("\U0001F5D1 Removing task - Enter task name: ")
-        print("")
+        print()             # Blank line
 
         for task in task_list:
             if remove_task == task["task"]:
@@ -44,97 +85,162 @@ class ToDoList(Task):
                 return ("Task removed \u2705 \n")
             
         return ("Not Found! \u274c \n")
-    
+    """
+    define a removing function that takes task name and search for it in the list and delete it if found it.
+    """
+
     @staticmethod
-    def showing_tasks():
-        high_task_list.clear()
-        middle_task_list.clear()
-        low_task_list.clear()
+    def show_tasks():
+        high_priority_task_list.clear()
+        middle_priority_task_list.clear()  # This is actually correct, otherwise old data would accumulate. Keep these lines.
+        low_priority_task_list.clear()
         
         for dict in task_list:
 
             if dict["priority"] == "High":
-                high_task_list.append(dict)
+                high_priority_task_list.append(dict)
 
             elif dict["priority"] == "Middle":
-                middle_task_list.append(dict)
+                middle_priority_task_list.append(dict)
                 
             elif dict["priority"] == "Low":
-                low_task_list.append(dict)
+                low_priority_task_list.append(dict)
 
             else:
-                print(f"Just use 'High, Middle or Low'. I can't accept {dict["priority"]} as priority. \u274c \n")
+                print(f"Just use 'High, Middle or Low'. I can't accept {dict['priority']} as priority. \u274c \n")
+        """
+        An static function (decorated) that shows tasks in sorted priority from high to low
+
+        ToDO: It need to be improved. I should make it better.
+        """
         
-        print("***Your To Do List in Priority***\n")
+        print("*** Your To Do List ***\n")
         print("First thing first - high priority: ")
-        for dict in high_task_list:
-            print(f"\u2022 Task: {dict["task"]}")
-            print(f"    description: {dict["description"]}\n")
+        for dict in high_priority_task_list:
+            print(f"\u2022 Task: {dict['task']}")
+            print(f"    description: {dict['description']}\n")
         
         print("Middle priority: ")
-        for dict in middle_task_list:
-            print(f"\u2022 Task: {dict["task"]}")
-            print(f"    description: {dict["description"]}\n")
+        for dict in middle_priority_task_list:
+            print(f"\u2022 Task: {dict['task']}")
+            print(f"    description: {dict['description']}\n")
         
         print("Low priority: ")
-        for dict in low_task_list:
-            print(f"\u2022 Task: {dict["task"]}")
-            print(f"    description: {dict["description"]}\n")
+        for dict in low_priority_task_list:
+            print(f"\u2022 Task: {dict['task']}")
+            print(f"    description: {dict['description']}\n")
+        
+        """
+        After sorting priorities in seperated lists, in this part will prints and shows tasks in priority.
+        """
+    
+    @staticmethod
+    def choose_file():
+        Tk().withdraw() # Hide the main window
+        file_path = filedialog.askopenfilename()
+        return file_path
+    
+    """
+    Here using Tkinter package to work with GUI to choosing file
+    """
+
+    @staticmethod
+    def where_to_save():
+        root = Tk()
+        root.withdraw()
+        folder_path = filedialog.askdirectory(
+            title="Select a Folder"
+        )
+        root.destroy()
+        return folder_path
+    
+    """ 
+    Also we used Tkinter package here because to be user friendly and easy to work with
+    """
 
 
-# Creating file with "your optional" name
+"""
+Create file with "your favorite name".
+You can change the file's name whenever you like:
+"""
+new_file = input("Do you want to create a new file? (y/n) -- ")
 
-# You can change the name of file what ever you like:
-your_to_do_list_name = "To-Do-List-file"
+""" Asks for new file or working with old file """
 
-# Creating file if it doesn't exist.
-try:
-    with open(f"D:\{your_to_do_list_name}.csv", "x"):
-        pass
-except FileExistsError:
-    print("The specific file is already exists.\n")
+
+
+if new_file.lower() in ("yes", "y"):
+
+    file_name = input("Enter your favorite name --> ")
+    your_to_do_list_name = file_name
+
+    """ You can name your file whatever you like """
+    
+    try:
+        path = ToDoList.where_to_save()
+        with open(f"{your_to_do_list_name}.csv", "x"):
+            pass
+    except FileExistsError:
+        print(f"\"{your_to_do_list_name}\" file is already exists.\n")
+    else:
+        print(f"\"{your_to_do_list_name}\".csv successfully created. \u2705 \n")
+    
+    """ Choose where you want to create and save your file """
+
+    """ But if your chose to work with previous files, the following code will be executed. """
 else:
-    print(f"{your_to_do_list_name}.csv successfully created. \u2705 \n")
+    file = ToDoList.choose_file()
+    with open("choosed_file", "a+"):
+            
+        process = True
 
+        while process:
+            print("1.Adding\n")
+            print("2.Removing\n")
+            print("3.Showing\n")
+            print("4.Save & Quit\n")
 
-# Opening file and Starting to work:
-with open(f"D:\{your_to_do_list_name}.csv", "a+") as file:
+            """ Main Panel """
 
-    process = True
+            user_choice = int(input("Select: "))
 
-    while process:
-        print("1.Adding\n")
-        print("2.Removing\n")
-        print("3.Showing\n")
-        print("4.Save & Quit\n")
+            match user_choice:
+                case 1:
+                    user_task_name = input("Task: ")
+                    user_task_description = input("Description: ")
+                    print("\u26A0 Attention: I only accept 'High, Middle or Low' words as priority [I only care about words not write formatting].")
+                    user_task_priority = input("Priority: ")
+                    dict_name = user_task_name
 
-        user_choose = int(input("Your command: "))
+                    """ In above, we take task information if user choose was 1 which means: 1.Adding """
 
-        match user_choose:
-            case 1:
-                task_name = input("Task: ")
-                my_task_description = input("Description: ")
-                print("\u26A0 Attention: I only accept 'High, Middle or Low' words as priority [I only care about words not write formatting].")
-                my_task_priority = input("Priority: ")
-                dict_name = task_name
+                    task = ToDoList(user_task_name, user_task_description, user_task_priority, dict_name)
+                    print(task.add_task())
 
-                task = ToDoList(task_name, my_task_description, my_task_priority, dict_name)
-                print(task.adding_task())
-                with open(f"D:\{your_to_do_list_name}.csv", "a") as file:
-                    writer = csv.DictWriter(file, fieldnames=["task", "description", "priority"])
-                    if file.tell() == 0:
-                        writer.writeheader()
-                    writer.writerow(task.dict_name)
-                print("")
+                    with open(f"{ToDoList.choose_file()}", "a") as file:
+                        writer = csv.DictWriter(file, fieldnames=["task", "description", "priority"])
+                        if file.tell() == 0:
+                            writer.writeheader()
+                        writer.writerow(task.dict_name)
 
-            case 2:
-                print(ToDoList.removing_task())
-
-            case 3:
-                ToDoList.showing_tasks()
+                    """ In here we used CSV package to save the results in a csv file """
                 
-            case 4:
-                process = False
+                case 2:
+                    print(ToDoList.removing_task())
 
-            case _:
-                print("\u274c Unexcepted. Try again!")
+                    """ This will removed """ 
+
+                case 3:
+                    ToDoList.show_tasks()
+
+                    """ This will show the tasks """
+                    
+                case 4:
+                    process = False
+
+                    """ This will saves and stops the program """
+
+                case _:
+                    print("\u274c Unexpected. Try again!")
+
+                    """ This note will prints if the user enters the incorrect input """
